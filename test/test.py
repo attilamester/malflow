8aee8c0ebbb7797b8ebb2cd7c3cfbc04f3eebcb1
Attila Mester <attilamester21@gmail.com>
2024-01-04 14:31:16 +0200

    Add pre-commit testing

--- test/test.py
+++ test/test.py
@@ -1,4 +1,10 @@
+import os
+import sys
 import unittest
+
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.join(PROJECT_ROOT, "src"))
+sys.path.append(os.path.join(PROJECT_ROOT, "test"))
 
 from cases.r2_scanner import TestR2Scanner
 
