cc54ddaf266b5f417c4f6922c8368e6dad8c72b8
Attila Mester <attilamester21@gmail.com>
2024-02-20 12:13:19 +0200

    Add GT distribution stats: 581 families

--- src/helpers/ground_truth.py
+++ src/helpers/ground_truth.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 import matplotlib.pyplot as plt
@@ -7,6 +8,8 @@ from util import config
 from util.logger import Logger
 
 config.load_env()
+
+BODMAS_METADATA_CSV = "/opt/work/bd/BODMAS/bodmas_metadata.csv"
 
 
 def read_bodmas_metadata(bodmas_meta_csv: str):
@@ -32,9 +35,25 @@ def create_ground_truth_bodmas(bodmas_meta_csv: str):
 
 def get_ground_truth_distribution(bodmas_meta_csv: str):
     df = read_bodmas_metadata(bodmas_meta_csv)
-    plt.title(f"BODMAS Family Distribution ({df['family'].nunique()} unique families)")
-    df["family"].value_counts().plot(kind="barh", figsize=(10, 60), logx=True)
+    ds_families = df["family"].dropna().str.lower()
+    value_counts = ds_families.value_counts()
+    total_samples_having_family = value_counts.sum()
+    buff = ""
+    for min_freq in [10, 50, 100, 500, 1000]:
+        gt_min = value_counts >= min_freq
+        having_gt_min = value_counts[gt_min]
+        pct = f"{having_gt_min.sum() / total_samples_having_family:.2%}"
+        buff += f"{len(having_gt_min):>5} families with at least {min_freq:>5} samples ({having_gt_min.sum()} total samples - {pct})\n"
+
+    plt.title(
+        f"BODMAS Family Distribution ({ds_families.nunique()} unique families, {total_samples_having_family} samples)\n{buff}")
+
+    value_counts.plot(kind="barh", figsize=(10, 60), logx=True)
     plt.savefig("BODMAS_ground_truth_family_distribution.pdf", bbox_inches='tight')
+
+    family_dict = {f: i for i, f in enumerate(sorted(list(ds_families.unique())))}
+    with open("BODMAS_ground_truth_family_dict.json", "w") as f:
+        json.dump(family_dict, f)
 
 
 def tmp_rename_png_files_to_contain_family(png_dir):
@@ -51,4 +70,5 @@ def tmp_rename_png_files_to_contain_family(png_dir):
 
 
 if __name__ == "__main__":
-    create_ground_truth_bodmas("/opt/work/bd/BODMAS/bodmas_metadata.csv")
+    # create_ground_truth_bodmas(BODMAS_METADATA_CSV)
+    get_ground_truth_distribution(BODMAS_METADATA_CSV)
