f77acadf0c7035c4e6bc6b0218eaa4da6f635412
csigabiga08 <csigabiga08@gmail.com>
2024-02-29 12:45:25 +0200

    fix dataloader to properly read ground truth labels; change PIL to opencv and Albumentations

--- src/bagnet_pytorch/dataloader.py
+++ src/bagnet_pytorch/dataloader.py
@@ -32,24 +32,36 @@ std = dataset_config["std"]
 
 dataset = pd.read_csv(os.path.join(data_dir, metadata_file), delimiter=",")
 
+def set_to_dict(s):
+    keys = list(s)
+    print("Number of unique filtered families:")
+    print(len(s))
+    values = range(0, len(s))
+    return {k: v for k, v in zip(keys, values)}
 
-# filtering out families with less than 50 samples:
-family_counts = dataset['family'].value_counts().to_frame()
-family_counts = family_counts[family_counts['count']<100]
-print(len(family_counts))
-rare_families = family_counts.index.values.tolist()
-dataset_filtered = dataset[~dataset['family'].isin(rare_families)]
+
+# filtering out families with less than 100 samples:
+family_counts = dataset['family'].value_counts()
+families_to_keep = family_counts[family_counts>=100].index
+# family_counts.to_csv('out.csv') 
+
+dataset_filtered = dataset[dataset['family'].isin(families_to_keep)]
 labels_filtered = dataset_filtered["family"]
+unique_filtered_labels = set(labels_filtered.unique())
+# dataset_filtered['family'].value_counts().to_csv('filtered_families.csv')
+
+BODMAS_FILTERED_LABEL_MAPPINGS = set_to_dict(unique_filtered_labels)
+print(BODMAS_FILTERED_LABEL_MAPPINGS)
+
 
 ds_train, ds_valid = train_test_split(dataset_filtered, stratify=labels_filtered, test_size=0.25)
-
 
 class BodmasDataset(torch.utils.data.Dataset):
     def __init__(self, df, transform = None):
         self.df = df
         self.transform = transform
         self.label_transform=lambda data: torch.tensor(data, dtype=torch.int),
-        self.class2index = BODMAS_LABEL_MAPPINGS
+        self.class2index = BODMAS_FILTERED_LABEL_MAPPINGS
 
     def __len__(self):
         return len(self.df)
@@ -59,27 +71,50 @@ class BodmasDataset(torch.utils.data.Dataset):
         label = self.class2index[line["family"]]
 
         #TODO: read with opencv instead? - depends on the augm library
-        image = PIL.Image.open(os.path.join(data_dir, images_dir, filename + '_False_False.png'))
+        # image = PIL.Image.open(os.path.join(data_dir, images_dir, filename + '_False_False.png'))
+        image = cv2.imread(
+            os.path.join(data_dir, images_dir, filename + '_224x224_True_True.png')
+        ) 
+
         if self.transform is not None:
-            image = self.transform(image)
+            # image = self.transform(image)
+            image = self.transform(image=image)["image"]  # transformations with Albumentations
         
         label = torch.tensor(label)
 
         return image, label
 
 
+# image transformations:
+normalize_augment = A.Compose(
+            [
+                A.ToFloat(    # [0..256] --> [0..1]
+                    max_value=256
+                ),  
+                A.Normalize(
+                    mean=mean, std=std, max_pixel_value=1.0, p=1.0
+                ),
+                # remove cropping - I added it so that the images fit in GPU memory; shouldn't be necessary for images equal to or smaller in size than 300x300
+                # A.crops.transforms.CenterCrop(
+                #     256, 256, always_apply=True, p=1.0
+                # ),
+                ToTensorV2(),
+            ]
+)
+
+
 
 # train set
 train_dataset = BodmasDataset(  # for RoadType and RoadCondition
     df = ds_train,
-    transform=transforms.Compose([transforms.CenterCrop(300), transforms.ToTensor(), transforms.Normalize(mean=mean, std=std)]),
+    transform = normalize_augment
 )
 
 
 train_sampler = torch.utils.data.RandomSampler(
     train_dataset,
     replacement=False,
-    num_samples=100,
+    num_samples=5000,
 )
 
 train_loader = torch.utils.data.DataLoader(
@@ -94,14 +129,13 @@ train_loader = torch.utils.data.DataLoader(
 # validation set
 valid_dataset = BodmasDataset(
     df = ds_valid,
-    # remove cropping - I added it so that the images fit in GPU memory; shouldn't be necessary for images equal to or smaller in size than 300x300
-    transform=transforms.Compose([transforms.CenterCrop(300), transforms.ToTensor(), transforms.Normalize(mean=mean, std=std)]), 
+    transform = normalize_augment
 )
 
 valid_sampler = torch.utils.data.RandomSampler(
     valid_dataset,
     replacement=False,
-    num_samples=200,
+    num_samples=500,
 )
 
 valid_loader = torch.utils.data.DataLoader(
