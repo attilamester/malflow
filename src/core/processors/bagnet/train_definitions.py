53905c0a895c3512befa11523e6275da429e3c64
Attila Mester <attilamester21@gmail.com>
2024-03-01 23:21:08 +0200

    Move cude build to separate build folder

---
+++ src/core/processors/bagnet/train_definitions.py
@@ -0,0 +1,21 @@
+import torch.utils.data
+
+
+def get_model() -> torch.nn.Module:
+    pass
+
+
+def get_train_dataset() -> torch.utils.data.Dataset:
+    pass
+
+
+def get_val_dataset() -> torch.utils.data.Dataset:
+    pass
+
+
+def get_train_loader() -> torch.utils.data.DataLoader:
+    pass
+
+
+def get_val_loader() -> torch.utils.data.DataLoader:
+    pass
