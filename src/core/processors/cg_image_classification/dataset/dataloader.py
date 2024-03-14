Correct mean-std calculation after filtering the ds. Dynamic batch size calculation

src/core/processors/cg_image_classification/dataset/dataloader.py
@@ -1,8 +1,6 @@
-import os
 from typing import Tuple, NamedTuple
 
 import albumentations as alb
-import cv2
 import pandas as pd
 import torch
 from albumentations.pytorch import ToTensorV2
@@ -22,7 +20,7 @@ class BodmasDataset(Dataset):
         self.dataset = dataset
         self.df = df
         self.transform = transform
-        self.iter_get_details = False
+        self.iter_details = False
 
     def __len__(self):
         return len(self.df)
@@ -31,7 +29,7 @@ class BodmasDataset(Dataset):
         """
         Setting True should be used only for debugging purposes - on train loop, this will raise an exception
         """
-        self.iter_get_details = flag
+        self.iter_details = flag
 
     class ItemDetailsPacked(NamedTuple):
         packed: bool
@@ -46,27 +44,20 @@ class BodmasDataset(Dataset):
 
     def __getitem__(self, index):
         line = self.df.iloc[index]
-        label = self.dataset.data_class2index[line["family"]]
+        label = self.dataset._data_class2index[line["family"]]
         label = torch.tensor(label)
 
-        def get_filename(md5):
-            return f"{md5}_{self.dataset.img_shape[0]}x{self.dataset.img_shape[1]}_True_True.png"
-
         def read_image(filename):
-            image = cv2.imread(os.path.join(self.dataset.img_dir_path, filename))
+            image = self.dataset.read_image(filename)
             if self.transform is not None:
                 image = self.transform(image=image)["image"]
             return image
 
-        if pd.notna(line["unpacked-md5"]):
-            md5 = line["unpacked-md5"]
-        else:
-            md5 = line["md5"]
-
-        filename = get_filename(md5)
+        md5 = self.dataset.get_row_id(line)
+        filename = self.dataset.get_filename(md5)
         image = read_image(filename)
 
-        if not self.iter_get_details:
+        if not self.iter_details:
             """
             Normal flow | during training / evaluating
             """
@@ -78,7 +69,7 @@ class BodmasDataset(Dataset):
 
         if pd.notna(line["unpacked-md5"]):
             # we already have the unpacked-md5 + image above
-            filename_original = get_filename(line["md5"])
+            filename_original = self.dataset.get_filename(line["md5"])
             image_original = read_image(filename_original)
             details = BodmasDataset.ItemDetails(md5, filename,
                                                 BodmasDataset.ItemDetailsPacked(True, line["md5"], filename_original,
@@ -108,15 +99,15 @@ def create_torch_bodmas_dataset_loader(dataset: ImgDataset, subset_df: pd.DataFr
     return ds, dl
 
 
-def create_bodmas_train_val_loader(dataset: ImgDataset, items_per_class: int, batch_size: int) \
+def create_bodmas_train_val_loader(dataset: ImgDataset, batch_size: int) \
         -> Tuple[
             BodmasDataset, DataLoader,
             BodmasDataset, DataLoader]:
-    Logger.info(f"Creating dataset & loader with items_per_class:{items_per_class}, batch_size:{batch_size}")
+    Logger.info(f"[Dataloader] Creating dataset loader with batch:{batch_size}")
 
-    df_filtered = dataset.filter_ground_truth(items_per_class)
-
-    ds_train, ds_valid = train_test_split(df_filtered, stratify=df_filtered["family"], test_size=0.25)
+    ds_train, ds_valid = train_test_split(dataset._data_df_gt_filtered,
+                                          stratify=dataset._data_df_gt_filtered["family"],
+                                          test_size=0.25)
 
     ds_tr, dl_tr = create_torch_bodmas_dataset_loader(dataset, ds_train, batch_size)
     ds_va, dl_va = create_torch_bodmas_dataset_loader(dataset, ds_valid, batch_size)

src/core/processors/cg_image_classification/dataset/dataset.py
@@ -22,84 +22,106 @@ class ImgDataset:
 
     augm: bool
 
-    _mean: Tuple[float, float, float] = field(default=None)
-    _std: Tuple[float, float, float] = field(default=None)
-    _num_classes: int = field(default=None)
-
     initialized: bool = field(default=False)
-    data_df_gt: pd.DataFrame = field(default=None)
-    data_df_gt_filtered: pd.DataFrame = field(default=None)
-    data_class2index: Dict[str, int] = field(default=None)
-    data_index2class: Dict[int, str] = field(default=None)
+    _data_num_classes: int = field(default=None)
+    _data_num_items: int = field(default=None)
+    _data_mean: Tuple[float, float, float] = field(default=None)
+    _data_std: Tuple[float, float, float] = field(default=None)
+    _data_df_gt: pd.DataFrame = field(default=None)
+    _data_df_gt_filtered: pd.DataFrame = field(default=None)
+    _data_class2index: Dict[str, int] = field(default=None)
+    _data_index2class: Dict[int, str] = field(default=None)
 
     @property
     def num_classes(self):
-        if self._num_classes is None:
-            raise Exception("num_classes is not set yet")
-        return self._num_classes
+        if self._data_num_classes is None:
+            raise Exception("Dataset is not initialized")
+        return self._data_num_classes
 
-    @num_classes.setter
-    def num_classes(self, value):
-        self._num_classes = value
+    @property
+    def size(self):
+        if self._data_num_items is None:
+            raise Exception("Dataset is not initialized")
+        return self._data_num_items
 
     @property
     def mean(self):
-        if self._mean is None:
-            self.calculate_mean_std()
-        return self._mean
-
-    @mean.setter
-    def mean(self, value):
-        Logger.warning(
-            f"Manually setting the `mean` value of the dataset ({self.img_dir_path}) from {self._mean} to {value}.")
-        self._mean = value
+        if self._data_mean is None:
+            raise Exception("Dataset is not initialized")
+        return self._data_mean
 
     @property
     def std(self):
-        if self._std is None:
-            self.calculate_mean_std()
-        return self._std
+        if self._data_std is None:
+            raise Exception("Dataset is not initialized")
+        return self._data_std
 
-    @std.setter
-    def std(self, value):
-        Logger.warning(
-            f"Manually setting the `std` value of the dataset ({self.img_dir_path}) from {self._std} to {value}.")
-        self._std = value
+    def read_image(self, filename):
+        image = cv2.imread(os.path.join(self.img_dir_path, filename))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        return image
+
+    # =================
+    # Below, methods are domain-specific. Maybe they should be moved to a subclass
+    # =================
 
     def get_ground_truth(self) -> pd.DataFrame:
-        if self.data_df_gt is None:
-            self.data_df_gt = pd.read_csv(self.ground_truth_path, delimiter=",")
-        return self.data_df_gt
+        if self._data_df_gt is None:
+            self._data_df_gt = pd.read_csv(self.ground_truth_path, delimiter=",")
+        return self._data_df_gt
+
+    def get_row_id(self, row: pd.Series) -> str:
+        if pd.notna(row["unpacked-md5"]):
+            return row["unpacked-md5"]
+        else:
+            return row["md5"]
+
+    def get_filename(self, md5):
+        return f"{md5}_{self.img_shape[0]}x{self.img_shape[1]}_True_True.png"
 
     def filter_ground_truth(self, items_per_class: int) -> pd.DataFrame:
-        Logger.info(f"Filtering ImgDataset with items_per_class:{items_per_class}")
         df = self.get_ground_truth()
         df_filtered = df_filter_having_at_column_min_occurencies(df, "family", items_per_class)
         family_index = list_to_dict_keys(list(df_filtered["family"].unique()))
 
-        self.num_classes = len(family_index)
-        self.data_df_gt_filtered = df_filtered
-        self.data_class2index = family_index
-        self.data_index2class = {v: k for k, v in family_index.items()}
+        self._data_num_classes = len(family_index)
+        self._data_df_gt_filtered = df_filtered
+        self._data_class2index = family_index
+        self._data_index2class = {v: k for k, v in family_index.items()}
         self.initialized = True
+
+        Logger.info(
+            f"[Dataset] Filtered ImgDataset with items_per_class:{items_per_class}. "
+            f"Samples: {len(df_filtered)} | Classes: {self.num_classes}")
+
+        self._calculate_mean_std()
+
         return df_filtered
 
-    def calculate_mean_std(self):
+    def _calculate_mean_std(self):
+        if not self.initialized:
+            raise Exception("Dataset is not initialized")
+
         channels_mean = [[], [], []]
         channels_std = [[], [], []]
-        for filename in os.listdir(self.img_dir_path):
-            if filename.endswith(".png"):
-                file_path = os.path.join(self.img_dir_path, filename)
+        for i, row in self._data_df_gt_filtered.iterrows():
+            md5 = self.get_row_id(row)
+            filename = self.get_filename(md5)
+            filepath = os.path.join(self.img_dir_path, filename)
 
-                image = cv2.imread(file_path, cv2.IMREAD_COLOR)
-                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            image = cv2.imread(filepath, cv2.IMREAD_COLOR)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-                for i, channel in enumerate(cv2.split(image)):
-                    channels_mean[i].append(np.mean(channel))
-                    channels_std[i].append(np.std(channel))
+            for i, channel in enumerate(cv2.split(image)):
+                channels_mean[i].append(np.mean(channel))
+                channels_std[i].append(np.std(channel))
 
-        self._mean = np.mean(channels_mean, axis=1) / 256  # 8-bit images
-        self._std = np.mean(channels_std, axis=1) / 256
+        self._data_mean = np.mean(np.array(channels_mean), axis=1) / 256  # 8-bit images
+        self._data_std = np.mean(np.array(channels_std), axis=1) / 256
+
+        Logger.info(f"[Dataset] Calculated mean&std on {len(channels_mean[0])} items from {self.img_dir_path}:\n"
+                    f"\tmean: {self._data_mean}\n"
+                    f"\t std: {self._data_std}")
 
 
 class Datasets(Enum):

src/core/processors/cg_image_classification/train_definitions.py
@@ -15,7 +15,7 @@ import torch.utils.data
 import torchvision
 
 from core.processors.cg_image_classification import paths
-from core.processors.cg_image_classification.dataset import Datasets
+from core.processors.cg_image_classification.dataset import Datasets, ImgDataset
 from core.processors.cg_image_classification.dataset.dataloader import create_bodmas_train_val_loader
 from core.processors.cg_image_classification.hparams import HPARAMS, get_hparam_value
 from core.processors.cg_image_classification.nn_model import bagnet17, bagnet9, bagnet33
@@ -28,30 +28,38 @@ from util.logger import Logger
 # =======================================================
 
 def get_model() -> torch.nn.Module:
-    if not Datasets.BODMAS.value.initialized:
-        init_train_valid_loader()
+    """depends on: DATASET"""
+
+    global MODEL, DATASET
+
+    if MODEL is not None:
+        return MODEL
+
+    if DATASET is None:
+        get_dataset()
 
     hp_model = get_hparam_value(HPARAMS.MODEL)
     hp_model_pretrained = get_hparam_value(HPARAMS.MODEL_PRETRAINED)
 
     if hp_model.startswith("bagnet"):
         if hp_model == "bagnet9":
-            return bagnet9(Datasets.BODMAS.value, pretrained=hp_model_pretrained)
+            MODEL = bagnet9(DATASET, pretrained=hp_model_pretrained)
         elif hp_model == "bagnet17":
-            return bagnet17(Datasets.BODMAS.value, pretrained=hp_model_pretrained)
+            MODEL = bagnet17(DATASET, pretrained=hp_model_pretrained)
         elif hp_model == "bagnet33":
-            return bagnet33(Datasets.BODMAS.value, pretrained=hp_model_pretrained)
+            MODEL = bagnet33(DATASET, pretrained=hp_model_pretrained)
     elif hp_model.startswith("resnet"):
         if hp_model == "resnet18":
-            m = torchvision.models.resnet18(pretrained=hp_model_pretrained)
-            m.fc = torch.nn.Linear(512 * 1, Datasets.BODMAS.value.num_classes)
-            return m
+            MODEL = torchvision.models.resnet18(pretrained=hp_model_pretrained)
+            MODEL.fc = torch.nn.Linear(512 * 1, DATASET.num_classes)
         elif hp_model == "resnet50":
-            m = torchvision.models.resnet50(pretrained=hp_model_pretrained)
-            m.fc = torch.nn.Linear(512 * 4, Datasets.BODMAS.value.num_classes)
-            return m
+            MODEL = torchvision.models.resnet50(pretrained=hp_model_pretrained)
+            MODEL.fc = torch.nn.Linear(512 * 4, DATASET.num_classes)
 
-    raise Exception(f"Unknown model: {hp_model}")
+    if MODEL is None:
+        raise Exception(f"Unknown model: {hp_model}")
+
+    return MODEL
 
 
 def get_model_info() -> str:
@@ -61,38 +69,42 @@ def get_model_info() -> str:
 
 
 def get_dataset_info() -> str:
-    ds = Datasets.BODMAS.value
+    if DATASET is None:
+        get_dataset()
+    ds = DATASET
     items_per_class = get_hparam_value(HPARAMS.DATA_MIN_ITEM_PER_CLASS)
-    batch_size = get_hparam_value(HPARAMS.DATA_BATCH_SIZE)
-    return f"Bodmas-{ds.img_shape[0]}x{ds.img_shape[1]}x{ds.img_color_channels}:{batch_size}:{items_per_class}"
+    return f"Bodmas-{ds.img_shape[0]}x{ds.img_shape[1]}x{ds.img_color_channels}:{BATCH_SIZE}:{items_per_class}"
 
 
 def get_train_dataset() -> torch.utils.data.Dataset:
-    if not dataset_loaded:
+    if not DATALOADER_LOADED:
         init_train_valid_loader()
-    return train_dataset
+    return TRAIN_DS
 
 
 def get_val_dataset() -> torch.utils.data.Dataset:
-    if not dataset_loaded:
+    if not DATALOADER_LOADED:
         init_train_valid_loader()
-    return valid_dataset
+    return VALID_DS
 
 
 def get_train_loader() -> torch.utils.data.DataLoader:
-    if not dataset_loaded:
+    if not DATALOADER_LOADED:
         init_train_valid_loader()
-    return train_loader
+    return TRAIN_LOADER
 
 
 def get_val_loader() -> torch.utils.data.DataLoader:
-    if not dataset_loaded:
+    if not DATALOADER_LOADED:
         init_train_valid_loader()
-    return valid_loader
+    return VALID_LOADER
 
 
 def get_logger() -> logging.Logger:
-    return Logger.get_logger('default')
+    l = Logger.get_logger('default')
+    _info = l.info
+    l.info = lambda msg: _info(f"[Main] {msg}")
+    return l
 
 
 def get_hparams() -> Dict[str, Union[int, float, bool, str]]:
@@ -102,38 +114,87 @@ def get_hparams() -> Dict[str, Union[int, float, bool, str]]:
 
 
 def target_class_translations() -> Dict[int, str]:
-    if not Datasets.BODMAS.value.initialized:
-        init_train_valid_loader()
-    return train_dataset.dataset.data_index2class
+    if DATASET is None:
+        get_dataset()
+    return DATASET._data_index2class
 
 
 # =======================================================
 # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 # =======================================================
 
-dataset_loaded = False
-(train_dataset, train_sampler, train_loader,
- valid_dataset, valid_sampler, valid_loader) = [None] * 6
+
+MODEL: torch.nn.Module = None
+DATASET: ImgDataset = None
+BATCH_SIZE: int = None
+
+DATALOADER_LOADED = False
+(TRAIN_DS, TRAIN_LOADER,
+ VALID_DS, VALID_LOADER) = [None] * 4
+
+
+# =======================================================
+# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+# =======================================================
+
+def get_dataset() -> ImgDataset:
+    """depends on: -"""
+
+    global DATASET, BATCH_SIZE
+    if DATASET is not None:
+        return DATASET
+
+    DATASET = Datasets.BODMAS.value
+    DATASET.filter_ground_truth(get_hparam_value(HPARAMS.DATA_MIN_ITEM_PER_CLASS))
+
+    BATCH_SIZE = get_batch_size(DATASET)
+
+    return DATASET
+
+
+def get_batch_size(ds: ImgDataset):
+    """depends on: -"""
+
+    # model = get_model()
+    # get_batch_size(model, torch.device("cpu"), (3, 300, 300), (Datasets.BODMAS.value.num_classes,), 1000, 8, 128)
+    batch_size = get_hparam_value(HPARAMS.DATA_BATCH_SIZE)
+    if ds.img_shape == (300, 300):
+        if batch_size > 16:
+            Logger.warning("[TrainDef] Batch size is too high for 300x300 image size")
+            return 16
+    elif ds.img_shape == (224, 224):
+        if batch_size > 32:
+            Logger.warning("[TrainDef] Batch size is too high for 224x224 image size")
+            return 32
+    elif ds.img_shape == (100, 100):
+        if batch_size > 64:
+            Logger.warning("[TrainDef] Batch size is too high for 30x30 image size")
+            return 64
+
+    return batch_size
 
 
 def init_train_valid_loader():
-    global dataset_loaded
-    global \
-        train_dataset, train_sampler, train_loader, \
-        valid_dataset, valid_sampler, valid_loader
+    """depends on: DATASET, BATCH_SIZE"""
 
-    if dataset_loaded:
+    global DATALOADER_LOADED
+    global \
+        TRAIN_DS, TRAIN_LOADER, \
+        VALID_DS, VALID_LOADER
+
+    if DATALOADER_LOADED:
         return
+
+    if DATASET is None:
+        get_dataset()
 
     files = os.listdir(paths.get_cg_image_classification_tb_log_dir())
     for file in files:
         if os.path.isdir(file) and get_model_info() in file and get_dataset_info() in file:
-            Logger.warning(f"Model and dataset already trained: {file}")
+            Logger.warning(f"[TrainDef] Model and dataset already trained: {file}")
 
-    (train_dataset, train_loader,
-     valid_dataset, valid_loader) = \
-        create_bodmas_train_val_loader(Datasets.BODMAS.value,
-                                       items_per_class=get_hparam_value(HPARAMS.DATA_MIN_ITEM_PER_CLASS),
-                                       batch_size=get_hparam_value(HPARAMS.DATA_BATCH_SIZE))
+    (TRAIN_DS, TRAIN_LOADER,
+     VALID_DS, VALID_LOADER) = \
+        create_bodmas_train_val_loader(DATASET, batch_size=BATCH_SIZE)
 
-    dataset_loaded = True
+    DATALOADER_LOADED = True