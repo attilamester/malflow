2abf8888135d80ae6edd947c670757c9a934a2b0
Attila Mester <attilamester21@gmail.com>
2024-03-14 18:18:02 +0200

    Improve env load display

--- src/util/config.py
+++ src/util/config.py
@@ -11,9 +11,15 @@ def load_env(path: str = None):
         else:
             env_path = path
         if os.path.isfile(env_path):
+            if path in __envs:
+                print(f"Env: already loaded [{env_path}]")
+                return
             load_dotenv(verbose=True, dotenv_path=env_path)
-            print(f"Loaded .env [{env_path}]")
+            print(f"Env: loaded [{env_path}]")
+            __envs[path] = True
         else:
             print(f"Not existing .env [{env_path}]")
     except Exception as e:
         print(f"Could not load .env file [{e}]")
+
+__envs = {}
