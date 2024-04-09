f525619e152aa6dc095f36968587bd89f392c6b4
Attila Mester <attilamester21@gmail.com>
2024-04-09 17:26:54 +0300

    Add pyproject, manifest, licence. Uploaded initial version to test.pypi

---
+++ src/malflow/__init__.py
@@ -0,0 +1,10 @@
+import argparse
+
+
+def main():
+    args = argparse.ArgumentParser()
+    args.add_argument("--input", help="Path to the executable file")
+
+
+if __name__ == "__main__":
+    main()
