Add new hparam: max-items-per-class Fix normalization of images when std is 0 (by using eps)

src/core/processors/cg_image_classification/train_definitions.py
@@ -115,7 +115,8 @@ def get_dataset_info() -> str:
     ds = DATASET
     items_per_class = get_hparam_value(HPARAMS.DATA_MIN_ITEM_PER_CLASS)
     augm = "augmen" if get_hparam_value(HPARAMS.DATA_AUGM) else "noaugm"
-    return f"Bodmas-{ds.img_shape[0]}x{ds.img_shape[1]}x{ds.img_color_channels}:{BATCH_SIZE}:{items_per_class}:{augm}"
+
+    return f"Bodmas-{ds.get_img_dir_name(0, 8)}:{ds.img_shape[0]}x{ds.img_shape[1]}x{ds.img_color_channels}:{BATCH_SIZE}:{items_per_class}:{augm}"
 
 
 def get_train_dataset() -> torch.utils.data.Dataset:
@@ -208,7 +209,8 @@ def get_dataset() -> ImgDataset:
 
     DATASET = Datasets.BODMAS.value
     DATASET.filter_ground_truth(get_hparam_value(HPARAMS.DATA_MIN_ITEM_PER_CLASS),
-                                get_hparam_value(HPARAMS.DATA_AUGM))
+                                get_hparam_value(HPARAMS.DATA_AUGM),
+                                get_hparam_value(HPARAMS.DATA_MAX_ITEMS_PER_CLASS))
 
     BATCH_SIZE = get_batch_size(DATASET)
 