fb10f2c8ebd3909cb9a40274677d0dbcc48fd496
Attila Mester <attilamester21@gmail.com>
2024-05-21 17:12:07 +0300

    Add insturction fmt (str format) parser

--- test/cases/instruction.py
+++ test/cases/instruction.py
@@ -16,6 +16,16 @@ class TestInstruction(unittest.TestCase):
         self.assertEqual(instruction_data.prefix, i.prefix)
         self.assertEqual(instruction_data.parameters, i.parameters)
 
+    def __test_instruction_fmt(self, instruction_data: InstructionData):
+        i = Instruction(instruction_data.disasm, b"0", [])
+
+        fmt = i.get_fmt()
+        i_parsed = Instruction.parse_fmt(fmt)
+        self.assertEqual(i.mnemonic, i_parsed.mnemonic)
+        self.assertEqual(i.prefix, i_parsed.prefix)
+        self.assertEqual(i.has_bnd, i_parsed.has_bnd)
+        self.assertEqual(i.parameters, i_parsed.parameters)
+
     def test_instructions(self):
         for item in INSTRUCTIONS:
             self.__test_instruction(item)
@@ -23,3 +33,7 @@ class TestInstruction(unittest.TestCase):
     def test_instruction_parameter_tokens(self):
         for input, expected in INSTRUCTION_PARAMETER_TOKENS:
             self.assertEqual(expected, InstructionParameter.split_into_parameter_tokens(input))
+
+    def test_instruction_fmt_parser(self):
+        for i_data in INSTRUCTIONS:
+            self.__test_instruction_fmt(i_data)
