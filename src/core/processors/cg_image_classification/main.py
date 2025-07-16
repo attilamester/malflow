ceb9c782ebcdf869f4e17a58ecd91557c2019b9a
Attila Mester <attilamester21@gmail.com>
2025-07-16 17:46:02 +0300

    Missed this file refactor: moved into malflow

--- src/core/processors/cg_image_classification/main.py
+++
@@ -1,5 +0,0 @@
-"""
-This file will be overwritten during docker build
-with the sample train script forked from pytorch/example:
-https://github.com/attilamester/pytorch-examples/blob/feature/83/imagenet/main.py
-"""
